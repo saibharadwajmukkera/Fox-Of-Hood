530c05c32486ae7f54e3441b6baf11cfdef179d9
saibharadwajmukkera <74823750+saibharadwajmukkera@users.noreply.github.com>
2024-11-05 20:27:42 -0500

    Phase 1 Commiting MenuBar.js

---
+++ src/componentsadded/MenuBar/MenuBar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './MenuBar.css';
+import { useNavigate } from 'react-router-dom';
+
+function MenuBar({ setView, handleLogout }) { 
+  const navigate = useNavigate();
+
+  const handleLogoutClick = () => {
+    handleLogout(); 
+    navigate('/');  
+  };
+
+  return (
+    <div className="menu-bar">
+      <div className="left-buttons">
+        <button className="menu-button" onClick={() => setView('portfolio')}>Portfolio</button>
+        <button className="menu-button" onClick={() => setView('buy-sell')}>Buy/Sell</button>
+      </div>
+      <div className="right-buttons">
+        <button className="menu-button">Profile</button>
+        <button className="menu-button" onClick={handleLogoutClick}>Logout</button>
+      </div>
+    </div>
+  );
+}
+
+export default MenuBar;
