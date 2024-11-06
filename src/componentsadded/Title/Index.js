ce489659b9278cee79ce8769ab6b95c7485374ae
saibharadwajmukkera <74823750+saibharadwajmukkera@users.noreply.github.com>
2024-11-06 16:43:51 -0500

    Updated Index.js

---
+++ src/componentsadded/Title/Index.js
@@ -0,0 +1,18 @@
+// import React from "react";
+// import {Head, HeadContainer, HeadOption} from './Title_style'
+
+// const Title = () =>{
+//     return (
+//         <Head>
+//             <HeadContainer>
+//                 Fox of Hood
+//             </HeadContainer>
+            
+//             <HeadOption>
+//                 Menu
+//             </HeadOption>
+//         </Head>
+//     )
+// }
+
+// export default Title
